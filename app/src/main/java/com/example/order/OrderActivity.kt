5af7e8db32d6b5d458716d0073e6c93e61838725
serser <Sergey21.95@gmail.com>
2022-06-13 19:37:45 +0300

    changed Order activity

--- app/src/main/java/com/example/order/OrderActivity.kt
+++ app/src/main/java/com/example/order/OrderActivity.kt
@@ -1,7 +1,9 @@
 package com.example.order
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.order.databinding.ActivityOrderBinding
 
 class OrderActivity : AppCompatActivity() {
@@ -13,5 +15,14 @@ class OrderActivity : AppCompatActivity() {
         var login = intent.getStringExtra("login")
         var password = intent.getStringExtra("password")
         binding.tvGreeting.text = "Hello, $login!"
+        binding.radioGroup.setOnCheckedChangeListener { radioGroup, i ->
+            if (binding.buttonCoffee.isChecked){
+                binding.spinnerCoffee.visibility = View.VISIBLE
+                binding.spinnerTea.visibility = View.INVISIBLE
+            }else {
+                binding.spinnerCoffee.visibility = View.INVISIBLE
+                binding.spinnerTea.visibility = View.VISIBLE
+            }
+        }
     }
 }
